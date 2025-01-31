bee21111cbb9411f092e1b88d5ce2a7faf50c9b4
ManethDulwan <manethdu@gmail.com>
2025-01-31 14:49:02 +0530

    jepsoft seo

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,8 +5,8 @@ import { SnackbarProvider } from "./providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Jepsoft",
-  description: "Best Software Company of the planet",
+  title: "Jepsoft | Smart Web & Software Solutions for Your Business",
+  description: "Jepsoft delivers top-notch web, software, and NFC solutions. From custom websites to smart business tools, we craft high-quality digital experiences",
 };
 
 export default function RootLayout({ children }) {
